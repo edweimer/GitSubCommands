dacd70998dd05efaaeb82c1839b44d0b088a8f43
Ed Weimer <eweimer@ccs-llc.com>
2025-04-23 16:41:17 -0400

    Checkout the temp branch for the base branch.
    Using the name of the base branch, prepend the "temp/" prefix and switch
    to the temp branch.
    
    See: #11

--- bash/for-git-subcommands.sh
+++ bash/for-git-subcommands.sh
@@ -1,6 +1,8 @@
 declare branch_base="";
+declare branch_temp="";
 
 declare branch_current=$(git branch | grep "^*" | cut -d " " -f 2-);
 declare client=${branch_current%%/*};
 
 branch_base=${branch_current#temp/};
+branch_temp="temp/${branch_base}";
